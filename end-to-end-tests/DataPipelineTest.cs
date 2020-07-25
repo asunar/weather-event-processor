7f2a2c68c99e0830e4d0c51ddd18307f352e5420
Alper Sunar <alper.sunar@autodesk.com>
2020-07-24 23:01:29 -0500

    Update manual test

--- end-to-end-tests/DataPipelineTest.cs
+++ end-to-end-tests/DataPipelineTest.cs
@@ -68,7 +68,7 @@ namespace DataPipelineTests
             return $"/aws/lambda/{lambdaName}";
         }
 
-        [Fact(Skip = "TODO: Fix permissions then enable")]
+        [Fact/*(Skip = "TODO: Fix permissions then enable")*/]
         public void DataPipelineEndtoEndTest()
         {
             string bucketName = ResolvePhysicalId("PipelineStartBucket");
--- manualTest.sh
+++ manualTest.sh
@@ -1,7 +1,7 @@
 # Upload test data to bucket
-aws s3 cp  src/BulkEvents/src/test/testData/sampledata.json s3://chapter6-data-pipeline-223200533796-us-east-1-start --profile personal
+aws s3 cp  src/BulkEvents/src/test/testData/sampledata.json s3://weather-event-processor-223200533796-us-east-1-start
 
-sam logs -n BulkEventsLambda --stack-name chapter6-data-pipeline -s 'yesterday' -e 'tomorrow' --profile personal
+sam logs -n BulkEventsLambda --stack-name weather-event-processor -s 'yesterday' -e 'tomorrow' --profile personal
 
-sam logs -n SingleEventLambda --stack-name chapter6-data-pipeline -s 'yesterday' -e 'tomorrow' --profile personal
+sam logs -n SingleEventLambda --stack-name weather-event-processor -s 'yesterday' -e 'tomorrow' --profile personal
 # Expected: Received weather event	
